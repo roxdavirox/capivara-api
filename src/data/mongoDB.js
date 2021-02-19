4922106f550495e8ceedc68b85b6a6d319bcc4e5
roxdavirox <roxdavirox@gmail.com>
2021-02-19 13:44:30 -0300

    feat: load dotenv & routes

--- src/data/mongoDB.js
+++ src/data/mongoDB.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const uri = process.env.MONGO_DB_CONNECTION;
+const uri = process.env.MONGO_DB_CONNECTION
 
 mongoose.connect(
   uri,
   { useNewUrlParser: true, useFindAndModify: false },
   (err) => {
-    const msg = err || '[capivara] banco de dados conectado';
+    const msg = err || '[capivara] banco de dados conectado'
     console.log(msg);
   },
 );
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
-module.exports = mongoose;
+module.exports = mongoose
--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ const createApp = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+const loadRoutes = require('./app/controllers/loadControllers')
+
 const app = createApp()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cors())
+
+require('dotenv').config()
+loadRoutes(app)
 
 const port = process.env.PORT || 3001
 
