63046f065e8f586c4b895e3a12bb2ad7e72d3a81
roxdavirox <roxdavirox@gmail.com>
2021-02-20 11:19:36 -0300

    feat: order controller get data orders value today

--- src/app/controllers/orderController.js
+++ src/app/controllers/orderController.js
@@ -2,22 +2,22 @@ const express = require('express')
 const pipedriveServices = require('../services/pipedriveServices')
 const blingServices = require('../services/blingServices')
 const orderServices = require('../services/orderServices')
+const { createOrder } = require('../../utils/orderXml')
 const router = express.Router()
 
 const orderController = {
   async getTotalValueToday(req, res) {
     try {
       const deals = await pipedriveServices.getAllWonDeals()
-      console.log('deals', deals)
       const orders = await blingServices.createOrders(deals)
       await orderServices.updateTotalValue(orders)
       const totalOrdersValueToday = await orderServices.getTotalvalueToday()
-      return res.send({ total: totalOrdersValueToday = 10 })
+      return res.send({ total: totalOrdersValueToday, orders })
     } catch(e) {
       return res.status(400)
         .send({ error: `Error on get total value: ${e}` });
     }
-  }
+  },
 }
 
 router.get('/today', orderController.getTotalValueToday)
